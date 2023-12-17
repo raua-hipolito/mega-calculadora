cf09ae1269bbbe3858bb8cfab608c61d644de2b4
Rauã <rauavasconcelos22@gmail.com>
2023-12-17 01:52:42 -0300

    ✅ Criando componente footer(rodapé) e css

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,25 @@
 import './App.css';
+import Footer from './components/Footer';
 import Header from './components/Header';
+import InfoBar from './components/InfoBar';
 
 function App() {
   return (
-    <Header />
+    <>
+
+      <Header />
+      <InfoBar
+        title="Para você que é MEI
+        ou quer se tornar MEI, calcule o valor da sua hora de trabalho:"
+      />
+
+      <InfoBar
+        title="Calcule o valor a ser cobrado para um projeto:"
+      />
+
+      <Footer />
+
+    </>
   );
 }
 
